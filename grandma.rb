c48ef00e4c8b3b020bf3ec17341ff988b0fcaeea
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-25 15:15:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
+<<<<<<< HEAD
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else if phrase != phrase.upcase
@@ -23,3 +24,14 @@ def speak_to_grandma(phrase)
   end
 end
 
+=======
+  if phrase == "I LOVE YOU GRANDMA"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elseif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NOT SINCE 1938!"
+  end
+end
+end
+>>>>>>> db3d923cd9a4979654f5ab83ef421c2a3048321b
